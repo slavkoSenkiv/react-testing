cbd12e3b193d03cdce51748996e27b6e0318488a
Yaroslav Senkiv <slavko.senkiv@gmail.com>
2024-09-22 08:30:34 +0200

    write presence tests

--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Greeting from './Greetings';
 
 describe('Greeting component', () => {
@@ -12,5 +13,33 @@ describe('Greeting component', () => {
     // Assert
     const helloWorldElement = screen.getByText('Hello World', { exact: false });
     expect(helloWorldElement).toBeInTheDocument();
+  });
+
+  test('renders "good to see you" if the button was NOT clicked', () => {
+    render(<Greeting />);
+    const outputElement = screen.getByText('good to see you', { exact: false });
+    expect(outputElement).toBeInTheDocument();
+  });
+
+  test('renders "changed" if the button WAS clicked', async () => {
+    //Arrange
+    render(<Greeting />);
+    //Act
+    const user = userEvent.setup();
+    await user.click(screen.getByRole('button'));
+    //Assert
+    const outputElement = screen.getByText('Changed!');
+    expect(outputElement).toBeInTheDocument();
+  });
+
+  test('does not render "good to see you" if the button WAS clicked', async () => {
+    //Arrange
+    render(<Greeting />);
+    //Act
+    const user = userEvent.setup();
+    await user.click(screen.getByRole('button'));
+    //Assert
+    const outputElement = screen.getByText('good to see you', { exact: false });
+    expect(outputElement).toBeNull();
   });
 });
--- src/components/Greetings.js
+++ src/components/Greetings.js
@@ -1,8 +1,22 @@
+import { useState } from 'react';
+import Output from './Output';
+
 const Greeting = () => {
+  const [changedText, setChangedText] = useState(false);
+
+  const changeTextHandler = () => {
+    if (!changedText) {
+      setChangedText(true);
+    } else {
+      setChangedText(false);
+    }
+  };
+
   return (
     <div>
       <h2>Hello World!</h2>
-      <p>It is good to see you</p>
+      {changedText ? <Output>Changed!</Output> : <Output>It is good to see you</Output>}
+      <button onClick={changeTextHandler}>Change Text!</button>
     </div>
   );
 };
