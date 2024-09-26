2d879496d367df60d600c66b8b2a67a20f1ca222
Yaroslav Senkiv <slavko.senkiv@gmail.com>
2024-09-26 20:41:19 +0200

    mock the data from fetch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Greeting from './components/Greetings';
 import './App.css';
+import Async from './components/Async';
 
 function App() {
   return (
     <div className="App">
       <Greeting />
+      <Async />
     </div>
   );
 }
